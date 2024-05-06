2487acffe2c766284d7471f29410c56892bc6f4d
OnlyRaydek <bayramsaid00@gmail.com>
2024-05-06 21:42:55 +0300

    update server

--- index.js
+++ index.js
@@ -12,35 +12,41 @@ console.log("Перевірка підключення до провайдера
 const provider = new ethers.providers.JsonRpcProvider(RPC);
 console.log("Провайдер ініціалізовано");
 
-// Інші частини коду
 const contractAddress = '0xa236fd48f30ad4dee145652a71912189855dd575';
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
 // Створення сервера
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 80;
 
-app.get('/', async (req, res) => {
-    res.send("Go to /circulating-supply");
-});
+app.get('/value/:param', async (req, res) => {
+    const param = req.params.param;
+    let result;
 
-app.get('/circulating-supply', async (req, res) => {
-    const totalSupply = await contract.totalSupply();
-    let excludedTotal = ethers.BigNumber.from(0);
+    switch (param) {
+        case 'totalsupply': {
+            const totalSupply = await contract.totalSupply();
+            result = ethers.utils.formatUnits(totalSupply, 18);
+            break;
+        }
+        case 'circulationsupply': {
+            const totalSupply = await contract.totalSupply();
+            let excludedTotal = ethers.BigNumber.from(0);
 
-    for (let address of excludedAddresses) {
-        const balance = await contract.balanceOf(address);
-        excludedTotal = excludedTotal.add(balance);
+            for (let address of excludedAddresses) {
+                const balance = await contract.balanceOf(address);
+                excludedTotal = excludedTotal.add(balance);
+            }
+
+            const circulatingSupply = totalSupply.sub(excludedTotal);
+            result = ethers.utils.formatUnits(circulatingSupply, 18);
+            break;
+        }
+        default:
+            result = 'Невідомий параметр';
     }
 
-    const circulatingSupply = totalSupply.sub(excludedTotal);
-    const formattedCirculatingSupply = ethers.utils.formatUnits(circulatingSupply, 18);
-
-    res.json({
-        totalSupply: ethers.utils.formatUnits(totalSupply, 18),
-        excludedTotal: ethers.utils.formatUnits(excludedTotal, 18),
-        circulatingSupply: formattedCirculatingSupply
-    });
+    res.send(result); // Повертаємо результат як простий текст
 });
 
 app.listen(port, () => {
